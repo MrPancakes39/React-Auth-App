9b8a752a31a29500d9a674d9de0ae90c37a76b34
MrPancakes39 <salmannhassan39@gmail.com>
2024-06-16 15:30:05 +0300

    Created Sign-in and Sign-up Pages

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,5 +1,73 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Link, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/sign-in")({
-  component: () => <div>Hello /sign-in!</div>,
+  component: () => <Page />,
 });
+
+function Page() {
+  return (
+    <div className="grid h-screen place-content-center">
+      <SignIn />
+    </div>
+  );
+}
+
+function SignIn() {
+  return (
+    <div className="mb-20 flex w-[400px] max-w-[calc(-2.5rem_+_100vw)] flex-col items-center justify-center overflow-hidden rounded-2xl border border-gray-300 bg-gray-100 shadow-lg">
+      <div className="flex w-full flex-col gap-8 rounded-b-lg border-b bg-white p-10">
+        <div className="text-center">
+          <h1 className="text-base font-bold text-gray-800">
+            Sign in to APP_NAME
+          </h1>
+          <p className="text-sm text-gray-500">
+            Welcome back! Please sign in to continue
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-800"
+            >
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="max-h-8 rounded-md border border-gray-300 px-1.5 py-3 text-sm outline-none focus-within:border-gray-400 focus-within:ring-2 focus-within:ring-gray-300 hover:border-gray-400"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-800"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="max-h-8 rounded-md border border-gray-300 px-1.5 py-3 text-sm outline-none focus-within:border-gray-400 focus-within:ring-2 focus-within:ring-gray-300 hover:border-gray-400"
+            />
+          </div>
+        </div>
+        <button
+          type="button"
+          className="transition- rounded-md border border-violet-600 bg-violet-500 px-3 py-2 text-sm text-white outline-none focus-within:border-gray-400 focus-within:ring-2 focus-within:ring-gray-300 hover:bg-violet-400"
+        >
+          Continue
+        </button>
+      </div>
+      <div className="w-full p-4 text-center text-sm text-gray-500">
+        Don't have an account?{" "}
+        <Link
+          to="/sign-up"
+          className="font-bold text-purple-600 hover:text-purple-500 hover:underline"
+        >
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
+}
